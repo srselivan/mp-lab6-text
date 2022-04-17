514c0cda53a84d62eb879ad46a257af8a81bf5ee
Sergey Selivankin <sergeyselivankin@gmail.com>
2022-04-17 10:26:55 +0300

    Add Ins*, Del*, Read & Print methods

--- headers/TText.h
+++ headers/TText.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "TNode.h"
 #include "TStack.h"
+#include <fstream>
 
 class TText {
 	TNode* pFirst, * pCurr;
@@ -34,4 +35,101 @@ public:
 		pCurr = pFirst;
 		st.Clear();
 	}
+
+	void InsNextLine(char str[]) {
+		if (pCurr) {
+			TNode* n = new TNode(str);
+			n->pNext = pCurr->pNext;
+			pCurr->pNext = n;
+		}
+	}
+
+	void InsNextSection(char str[]) {
+		if (pCurr) {
+			TNode* n = new TNode(str);
+			n->pDown = pCurr->pNext;
+			pCurr->pNext = n;
+		}
+	}
+
+	void InsDownLine(char str[]) {
+		if (pCurr) {
+			TNode* n = new TNode(str);
+			n->pNext = pCurr->pDown;
+			pCurr->pDown = n;
+		}
+	}
+
+	void InsDownSection(char str[]) {
+		if (pCurr) {
+			TNode* n = new TNode(str);
+			n->pDown  = pCurr->pDown;
+			pCurr->pDown = n;
+		}
+	}
+
+	void DelNextLine() {
+		if (pCurr) {
+			if (pCurr->pNext) {
+				TNode* n = pCurr->pNext;
+				pCurr->pNext = n->pNext;
+				delete n;
+			}
+		}
+	}
+
+	void DelDownLine() {
+		if (pCurr) {
+			if (pCurr->pDown) {
+				TNode* n = pCurr->pDown;
+				pCurr->pDown = n->pNext;
+				delete n;
+			}
+		}
+	}
+
+	TNode* ReadRec(std::ifstream& ifs) {
+		TNode* pHead, * tmp;
+		char buf[81];
+
+		while (!ifs.eof()) {
+			ifs.getline(buf, 80, '\n');
+			if (buf[0] == '}') {
+				break;
+			}
+			else if (buf[0] == '{') {
+				tmp->pDown = ReadRec(ifs);
+			}
+			else {
+				TNode* p = new TNode(buf);
+				if (pHead == NULL) {
+					pHead = tmp = p;
+				}
+				else {
+					tmp->pNext = p;
+					tmp = p;
+				}
+			}
+		}
+
+		return pHead;
+	}
+
+	void Read(char* fn) {
+		std::ifstream ifs(fn);
+		pFirst = ReadRec(ifs);
+	}
+
+	void PrintRec(TNode* p) {
+		if (p) {
+			std::cout << p->str << std::endl;
+			PrintRec(p->pDown);
+			PrintRec(p->pNext);
+		}
+	}
+
+	void Print() {
+		PrintRec(pFirst);
+	}
+
 };
